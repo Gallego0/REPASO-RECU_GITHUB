faf475e4bc441bfd2094934cf15e836c279f3d0d
Gallego0 <sg9787188@gmail.com>
2023-06-13 20:51:13 +0200

    Subir cambios a las clases

--- src/Edades.java
+++ src/Edades.java
@@ -1,4 +1,49 @@
 
 public class Edades {
+	
+	int [] edades = {12, 15, 67, 43};
+	
+	
+	public Edades(int[] edades) {
+		
+		this.edades = edades;
+	}
 
+	public int contarMenores (int [] edades) {
+		
+		int contador=0;
+		
+		for (int num : edades) {
+			if (num<18) {
+				contador++;
+			}
+		}
+		return contador;
+		
+	}
+	
+	public int contarMayores(int [] edades) {
+		
+		int contador=0;
+		
+		for (int num : edades) {
+			if (num>=18 && num<=65) {
+				contador++;
+			}
+		}
+		return contador;
+		
+	}
+	public int contarJubilados (int [] edades) {
+	
+	int contador=0;
+	
+	for (int num : edades) {
+		if (num>65) {
+			contador++;
+		}
+	}
+	return contador;
+	
+	}
 }
--- src/PrincipalApp.java
+++ src/PrincipalApp.java
@@ -1,4 +1,20 @@
-
+import java.util.*;
 public class PrincipalApp {
+	
+	static Scanner sc = new Scanner (System.in);
+	public static void main (String [] args) {
+		
+		int [] edades = {12, 15, 67, 43};
+		
+		Edades contarEdades = new Edades(edades);
+		
+		int contarMenores = contarEdades.contarMenores(edades);
+		int contarAdultos = contarEdades.contarMayores(edades);
+		int contarJubilados = contarEdades.contarJubilados(edades);
+		
+		System.out.println("Los menores son:" + contarMenores);
+		System.out.println("Los mayores son:" + contarAdultos);
+		System.out.println("Los jubilados son:" + contarJubilados);
 
+	}
 }
